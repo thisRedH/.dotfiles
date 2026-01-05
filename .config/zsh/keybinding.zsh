f4932f08c280f13520e7474205f103a0c98ea8a7
thisRedH <114261766+thisRedH@users.noreply.github.com>
2026-01-05 01:02:01 +0100

    Add fallback keybindings

--- .config/zsh/keybinding.zsh
+++ .config/zsh/keybinding.zsh
@@ -12,12 +12,18 @@ bindkey "^[[5D" backward-word               # [Ctrl-LeftArrow]
 bindkey "^H" backward-kill-word             # [Ctrl-Backspace]
 bindkey "^[[3;5~" kill-word                 # [Ctrl-Delete]
 
-bindkey "$(tput khome)" beginning-of-line   # [HOME]
+bindkey "$(tput khome)" beginning-of-line   # [Home]
+bindkey "^[[H" beginning-of-line            # [Home]
 bindkey "$(tput kend)" end-of-line          # [End]
+bindkey "^[[F" end-of-line                  # [End]
 bindkey "$(tput kdch1)" delete-char         # [Delete]
+bindkey "^[[3~" delete-char                 # [Delete]
 bindkey "$(tput kpp)" up-line-or-history    # [Page Up]
+bindkey "^[[5~" up-line-or-history          # [Page Up]
 bindkey "$(tput knp)" down-line-or-history  # [Page Down]
+bindkey "^[[6~" down-line-or-history        # [Page Down]
 bindkey "$(tput kich1)" overwrite-mode      # [Insert]
+bindkey "^[[2~" overwrite-mode              # [Insert]
 
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
